1d9e77ba1a7c4ce6b0fdd426c3bf2d73c9bb0cd0
heroic <himbazasemu23@gmail.com>
2025-03-26 20:00:39 +0200

    int

--- heroic/task1.py
+++ heroic/task1.py
@@ -1,10 +1,15 @@
 
-name=input("enter your name")
-age=int(input("enter your age"))
+name=input("enter your name : ")
+age=int(input("enter your age   :"))
 def check(Name,Age):
     if Age>=20 :
         print(Name, "is adult" )
+        print(Age, "years old" )
+        
     elif Age>=10 and Age<=20 :
         print(Name,"is tenager") 
+        print(Age, "years old" )
     else:
-        print(Name,"is child")      
+        print(Name,"is child")  
+        print(Age, "years old" )    
+check(name,age)       
