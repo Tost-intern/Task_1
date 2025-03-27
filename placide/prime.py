7590d2f8025fb095f61f8e085c4ceb2d883d5256
cbsdevl <166548181+cbsdevl@users.noreply.github.com>
2025-03-27 10:58:55 +0200

    Update prime.py

--- placide/prime.py
+++ placide/prime.py
@@ -1,14 +1,21 @@
 
     #prime num
-num = int(input("Enter a number: "))
-if num > 1:
 
 
-        for i in range(2, num):
-            if (num % i) == 0:
-                print(num, "is not a prime number")
+def prime(num):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                     print(num, "is not a prime number")
                 break
+            else:
+             print(num, "is a prime number")
+        
         else:
-            print(num, "is a prime number")
-else:
-        print(num, "is not a prime number")
+           print(num, "is not a prime number")
+
+num = int(input("Enter a number: "))
+
+
+prime(num)
+
