cc60dc65707e451bcbb713663cfcfb84100a831d
pithom <cfeddx6@gmail.com>
2025-03-27 11:04:18 +0200

    eddy

---
+++ MUVUNYI/prime.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+for num in range(1, 101):
+    if num > 90:  
+        break
+    if is_prime(num):
+        print(num)
